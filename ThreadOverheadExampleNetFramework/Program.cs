5e4be3044372b05595919d3b570c22288de1fb66
Vasile Cuzmin <vcuzmin@totalsoft.ro>
2020-06-19 11:25:35 +0300

    add another files

---
+++ ThreadOverheadExampleNetFramework/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ThreadOverheadExampleNetFramework
+{
+    class Program
+    {
+        static ManualResetEvent wakeThreads = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            ThreadOverhead();
+
+            Console.Read();
+        }
+
+        private static void ThreadOverhead()
+        {
+            const Int32 OneMB = 1024 * 1024;
+            int threadNum = 0;
+            try
+            {
+                while (true)
+                {
+                    Thread t = new Thread(WaitOnEvent);
+                    t.Start(wakeThreads);
+                    Console.WriteLine("{0}: {1}MB", ++threadNum,
+                        Process.GetCurrentProcess().PrivateMemorySize64 / OneMB);
+                }
+            }
+            catch (OutOfMemoryException)
+            {
+                Console.WriteLine("Out of memory after {0} threads", threadNum);
+                Debugger.Break();
+                wakeThreads.Set(); // Release all the threads
+            }
+        }
+
+        private static void WaitOnEvent(object obj)
+        {
+            wakeThreads.WaitOne();
+        }
+    }
+}
